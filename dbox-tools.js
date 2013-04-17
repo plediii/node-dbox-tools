6895b0cb6e5c51ec926cc988d63c13ef7dadbc0c
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2013-04-17 01:07:35 +0000

    Implement array targets for updateMetadata.

--- dbox-tools.js
+++ dbox-tools.js
@@ -6,34 +6,54 @@ var metadatamod = require('./metadata')
 var updateMetadata = exports.updateMetadata = function (cli, metadata, targets, cb) {
     metadata = metadatamod.fileset(metadata);
 
-    var path;
-    if (targets.hasOwnProperty('path')) {
-	path = targets.path;
-    }
-    else if (typeof targets === 'string') {
-	path = targets;
+    var updateTarget = function (target, cb) {
+	var path;
+	if (target.hasOwnProperty('path')) {
+	    path = target.path;
+	}
+	else if (typeof target === 'string') {
+	    path = target;
+	}
+	else {
+	    return cb('unknown target type: ' + json.stringify(target));
+	}
+	var options = {};
+	if (metadata.hasOwnProperty(path) && metadata[path].hasOwnProperty('hash')) {
+	    options.hash = metadata[path].hash;
+	}
+	return cli.metadata(path, options, function (err, meta) {
+	    if (err === 200) {
+		metadatamod.changePath(metadata, path, meta)
+	    }
+	    else if (err === 404) {
+		metadatamod.rm(metadata, path);
+	    }
+	    else if (err === 304) {}
+	    else {
+		return cb('unexpected error getting client metadata for path ' + path + ' : ' + err);
+	    }
+	    return cb(null, metadata);
+	});
+    };
+
+    if (targets.hasOwnProperty('path') || typeof targets === 'string') {
+	return updateTarget(targets, cb);
     }
     else {
-	throw 'unexpected targets type: ' + typeof targets;
+	return (function updateLoop (targets) {
+	    if (targets.length < 1) {
+		return cb(null, metadata);
+	    }
+	    return updateTarget(_.head(targets), function (err) {
+		if (err) {
+		    return cb(err);
+		}
+		else {
+		    updateLoop(_.rest(targets));
+		}
+	    });
+	})(targets);
     }
-    var oldMeta = null;
-    var options = {};
-    if (metadata.hasOwnProperty(path) && metadata[path].hasOwnProperty('hash')) {
-	options.hash = metadata[path].hash;
-    }
-    return cli.metadata(path, options, function (err, meta) {
-	if (err === 200) {
-	    metadatamod.changePath(metadata, path, meta)
-	}
-	else if (err === 404) {
-	    metadatamod.rm(metadata, path);
-	}
-	else if (err === 304) {}
-	else {
-	    return cb('unexpected error getting client metadata for path ' + path + ' : ' + err);
-	}
-	return cb(null, metadata);
-    });
 };
 
 exports.delta = function (cli, cursor, options) {
